764d7f45dc1a8d082813b9f142f37c0b7f426b2c
Ana Clara Queiroz Bezerra <20231094010044@ifrn.local>
2026-09-09 17:22:06 -0300

    tentativa

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,27 @@
-from django.contrib import admin
-from django.urls import path, include
 
-urlpatterns = [
-    path('', admin.site.urls),
+from django.shortcuts import render, get_object_or_404
+from .models import Projeto
 
-]
+
+def lista_projetos(request):
+    projetos = Projeto.objects.all()
+
+    return render(
+        request,
+        'app/lista_projetos.html',
+        {
+            'projetos': projetos
+        }
+    )
+
+
+def detalhe_projeto(request, id):
+    projeto = get_object_or_404(Projeto, id=id)
+
+    return render(
+        request,
+        'app/detalhe_projeto.html',
+        {
+            'projeto': projeto
+        }
+    )
